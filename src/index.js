44c4ff82a1e3d60c0dbcf5757f5a1a50d48987da
Robert Chen <bergintonatcapital@gmail.com>
2023-07-27 01:58:06 -0400

    Created hamburger & setup modal outline

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 import "./styles/website.css";
 
+const hamburger = document.querySelector('.hamburger');
+const sideNavbar = document.querySelector('.side-nav');
+
+hamburger.addEventListener('click', () => {
+   hamburger.classList.toggle('active');
+   sideNavbar.classList.toggle('active');
+})
